3f6eee6fb65565bc5f1834633e34321e89c00846
Miłosz Bargieł <miloszbargiel@gmail.com>
2024-10-16 21:38:16 +0200

    Reorder supervision in Todo.System

--- lib/todo/system.ex
+++ lib/todo/system.ex
@@ -1,6 +1,6 @@
 defmodule Todo.System do
   def start_link(n_workers \\ 3) do
-    children = [Todo.Cache, {Todo.Database, n_workers}, Todo.ProcessRegistry]
+    children = [Todo.ProcessRegistry, {Todo.Database, n_workers}, Todo.Cache]
     Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
