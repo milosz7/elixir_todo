288b220b8f5f35215a8ec8fbafcc7a639ce94125
Miłosz Bargieł <miloszbargiel@gmail.com>
2024-10-16 21:37:18 +0200

    Add Todo ProcessRegistry

--- lib/todo/system.ex
+++ lib/todo/system.ex
@@ -1,5 +1,6 @@
 defmodule Todo.System do
   def start_link(n_workers \\ 3) do
-    Supervisor.start_link([Todo.Cache, {Todo.Database, n_workers}], strategy: :one_for_one)
+    children = [Todo.Cache, {Todo.Database, n_workers}, Todo.ProcessRegistry]
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
